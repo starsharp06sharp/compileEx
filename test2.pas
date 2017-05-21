2561cf12e6d018f6915f794adde6fc7c62936f67
starsharp06sharp <zhenglei1996@live.cn>
2017-05-21 20:15:24 +0800

    new examples

--- test2.pas
+++ test2.pas
@@ -1,6 +1,14 @@
-function E
 begin
-    a := 1;
-    b := 2;
-    ```
-end;
+  integer k;
+  integer m;
+  integer function F(n);
+    begin
+      integer t;
+      if n<=0 then F:=1
+      else F:=n*F(n-1)
+    end;
+  integer t;
+  read(m);
+  k:=F(m);
+  write(k)
+end
---
+++ test2.pro
@@ -0,0 +1,6 @@
+pname: F
+ptype: ints
+plev: 1
+fadr: 2
+ladr: 3
+
